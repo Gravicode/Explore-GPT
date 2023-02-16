79165b28c1bf04887c18e939c196eb77e733063a
Janu <januartha.rn@gmail.com>
2023-02-16 16:46:10 +0700

    init

---
+++ src/ExploreGPT/Data/AppConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace ExploreGPT.Data
+{
+    public class AppConstants
+    {
+        public static string OpenAIKey = "";
+    }
+}
---
+++ src/ExploreGPT/Program.cs
@@ -0,0 +1,46 @@
+using ExploreGPT.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using MudBlazor.Services;
+using OpenAI.GPT3;
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Managers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddMudServices();
+var configBuilder = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile("appsettings.json", optional: false);
+IConfiguration Configuration = configBuilder.Build();
+
+AppConstants.OpenAIKey = Configuration["OpenAIKey"];
+builder.Services.AddOpenAIService(settings => { settings.ApiKey = AppConstants.OpenAIKey; });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
